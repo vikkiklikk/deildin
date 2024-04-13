bf7a9e6906fd05a1843b9b14242e51392c3cac12
Viktor Birgisson <viktor.birgisson@gmail.com>
2024-04-13 09:55:20 +0000

    fixed indentations

--- backend/main.py
+++ backend/main.py
@@ -16,14 +16,14 @@ def create_contact():
     email = request.json.get("email")
 
     if not first_name or not last_name or not email:
-       return (jsonify({"message": "You must include a first name, last name and email"}), 400) #400 a bad request
+        return (jsonify({"message": "You must include a first name, last name and email"}), 400) #400 a bad request
     
     new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
     try:
-       db.session.add(new_contact)
-       db.session.commit()
+        db.session.add(new_contact)
+        db.session.commit()
     except Exception as e:
-       return jsonify({ "message": str(e)}), 400
+    return jsonify({ "message": str(e)}), 400
     
     return jsonify({"message": "User created!"}), 201
 
@@ -47,19 +47,18 @@ def update_contact(user_id):
 def delete_user(user_id):
     contact = Contact.query.get(user_id)
 
-    if not contact:
-       return jsonify({"message": "User not found"}), 404
+    if not contact: return jsonify({"message": "User not found"}), 404
     
     db.session.delete(contact)
     db.session.commit()
 
     return jsonify({"message": "User deleted"}), 200
-           
+
 
 # Spin up the database if it does not exist
 if __name__ == "__main__":
-  with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
 
-  app.run(debug=True)
+app.run(debug=True)
